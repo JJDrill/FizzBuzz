6ed76fd2ca487158ac18c5f8c25781d0d4751268
J. Drill <JJDrill@hotmail.com>
2015-10-29 09:57:59 -0600

    Added final case to print number for the rest

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ module.exports = {
   	  return "FizzBuzz";
   	} else if (number % 3 === 0) {
   		return "Fizz";
+  	} else if (number % 5 === 0) {
+  		return "Buzz";
+  	} else {
+  		return	number;
   	}
 	}
+	
 }
--- test/test.js
+++ test/test.js
@@ -7,11 +7,21 @@ describe('FizzBuzz', function(){
     expect(code.FizzBuzz(9)).to.equal("Fizz");
     expect(code.FizzBuzz(18)).to.equal("Fizz");
   });
-  it('should print "Buzz" if it can be divided by 5');
+  it('should print "Buzz" if it can be divided by 5', function(){
+    expect(code.FizzBuzz(5)).to.equal("Buzz");
+    expect(code.FizzBuzz(10)).to.equal("Buzz");
+    expect(code.FizzBuzz(20)).to.equal("Buzz");
+  });
   it('should print "FizzBuzz" if it can be divided by both 3 and 5', function(){
     expect(code.FizzBuzz(15)).to.equal("FizzBuzz");
     expect(code.FizzBuzz(30)).to.equal("FizzBuzz");
     expect(code.FizzBuzz(45)).to.equal("FizzBuzz");
   });
-  it('should print the number if it is not divisible by either');
+  it('should print the number if it is not divisible by either', function(){
+    expect(code.FizzBuzz(1)).to.equal(1);
+    expect(code.FizzBuzz(2)).to.equal(2);
+    expect(code.FizzBuzz(4)).to.equal(4);
+    expect(code.FizzBuzz(7)).to.equal(7);
+    expect(code.FizzBuzz(8)).to.equal(8);
+  });
 });
