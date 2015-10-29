18cdb00c5eef38512d21e03b76fd73e4d3376b30
J. Drill <JJDrill@hotmail.com>
2015-10-29 09:50:31 -0600

    FizzBuzz test case working

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 module.exports = {
+
 	FizzBuzz: function(number) {
-  	if (number % 3 && number % 5) {
-  	  console.log(FizzBuzz);
+  	if ((number % 3 === 0) && (number % 5 === 0)) {
+  	  return("FizzBuzz");
   	}
 	}
 }
--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ var expect = require('chai').expect;
 describe('FizzBuzz', function(){
 	it('should print "Fizz" if it can be divided by 3');
   it('should print "Buzz" if it can be divided by 5');
-  it('should print "FizzBuzz" if it can be divided by both 3 and 5');
+  it('should print "FizzBuzz" if it can be divided by both 3 and 5', function(){
+    expect(code.FizzBuzz(15)).to.equal("FizzBuzz");
+  });
   it('should print the number if it is not divisible by either');
 });
