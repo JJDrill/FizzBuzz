0b6b3db3f042d93c245f41d49ea05c9d63966e83
J. Drill <JJDrill@hotmail.com>
2015-10-29 09:43:12 -0600

    Initial checkin

---
+++ main.js
@@ -0,0 +1,7 @@
+module.exports = {
+	FizzBuzz: function(number) {
+  	if (number % 3 && number % 5) {
+  	  console.log(FizzBuzz);
+  	}
+	}
+}
---
+++ test/test.js
@@ -0,0 +1,9 @@
+var code = require('../main');
+var expect = require('chai').expect;
+
+describe('FizzBuzz', function(){
+	it('should print "Fizz" if it can be divided by 3');
+  it('should print "Buzz" if it can be divided by 5');
+  it('should print "FizzBuzz" if it can be divided by both 3 and 5');
+  it('should print the number if it is not divisible by either');
+});
